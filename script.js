c56164fda7c5ca6ef6ef69f312a787f75920c972
samsdogjack <105887707+samsdogjack@users.noreply.github.com>
2022-09-02 12:16:37 -0400

    change dice name

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-var player 1
+let player= "player";
+
+
+//Math.floor(Math.random() * 7);
+
+function rollTheDice() {
+    setTimeout(function () {
+        var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+        var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+        document.querySelector(".img1").setAttribute("src",
+            "dice" + randomNumber1 + ".png");
+
+        document.querySelector(".img2").setAttribute("src",
+            "dice" + randomNumber2 + ".png");
+
+        if (randomNumber1 === randomNumber2) {
+            document.querySelector("h1").innerHTML = "Draw!";
+        }
+
+        else if (randomNumber1 < randomNumber2) {
+            document.querySelector("h1").innerHTML
+                            = (player2 + " WINS!");
+        }
+
+        else {
+            document.querySelector("h1").innerHTML
+                            = (player1 + " WINS!");
+        }
